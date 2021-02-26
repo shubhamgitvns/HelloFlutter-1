e3c5a915642cb1ee87bd2b981b326d07121b1238
champaksworld <champaksworld@users.noreply.github.com>
2021-02-26 18:42:30 +0530

    Used and asset image instead of an internet image. Added into image folder and accessed via pubspec.yaml

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -17,8 +17,8 @@ void main() {
         body:
 
         Center(child: Image(
-          image: NetworkImage(
-              "https://3.bp.blogspot.com/-py5FbTZgvjo/YDi1bsQq16I/AAAAAAAACB0/BxejbJBcHA4AVfkB33WYC3YlVmxElM7BwCK4BGAYYCw/s1600/Varanasi%2BSoftware%2BJunction%2BPhone%2BLogo.png"),
+          image: AssetImage(
+              "images/vsjpl.png"),
         ),
       ),
     ),
