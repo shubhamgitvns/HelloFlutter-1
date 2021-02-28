596d2a18e0ea4d57924b3c880bf0b1967fe8fc06
champaksworld <champaksworld@users.noreply.github.com>
2021-02-28 12:19:34 +0530

    Container, Margin and Padding

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -5,16 +5,24 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(VSJApp());
 }
-class VSJApp extends StatelessWidget
-{
+
+class VSJApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return MaterialApp(
-home:Scaffold(
-  backgroundColor: Colors.blueAccent,
+      home: Scaffold(
+        backgroundColor: Colors.teal,
 
-    ),
+        body: SafeArea(
+          child: Container(
+            width: 100,
+              height: 100,
+              margin: EdgeInsets.all(10),
+              padding: EdgeInsets.symmetric(vertical: 10,horizontal: 20),
+              color: Colors.white, child: Text("Varanasi Software Junction")),
+        ),
+      ),
     );
   }
 }
