06508decf48d221be05c7ddf4591f85b388e9117
champaksworld <champaksworld@users.noreply.github.com>
2021-02-28 13:39:33 +0530

    mainAxisAlignment: MainAxisAlignment.end,

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -15,7 +15,7 @@ class VSJApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
-            verticalDirection: VerticalDirection.up,
+            mainAxisAlignment: MainAxisAlignment.end,
             children: [
               Container(
                 width: 100,
