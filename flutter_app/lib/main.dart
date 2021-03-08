ba16d2b628544e64995b16a9b49a0eaf15500593
champaksworld <champaksworld@users.noreply.github.com>
2021-03-08 23:18:53 +0530

    Adding a package from pub.dev
    Check pubspec.yaml and main.dart

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,20 +1,38 @@
 import 'dart:ui';
-
+import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(VSJApp());
+  runApp(MusicApp());
 }
-class VSJApp extends StatelessWidget
-{
+
+class MusicApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+    String firstword = nouns.first;
+    String lastword = nouns.last;
     return MaterialApp(
-home:Scaffold(
-  backgroundColor: Colors.blueAccent,
+      home: Scaffold(
+        appBar: AppBar(
+          title:Text( firstword + " " + lastword,style: TextStyle(fontSize: 24),),
+          centerTitle: true,
+          backgroundColor: Colors.teal[200],
+        ),
+        backgroundColor: Colors.teal[900],
+        body: SafeArea(
+          child: Column(
+            children: [
 
-    ),
+              Center(
+                child: Image(
+                  image: AssetImage("images/vsjpl.png"),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
