7e08ce30c268eead1497337e029ae0f67760bde9
champaksworld <champaksworld@users.noreply.github.com>
2021-03-13 13:30:13 +0530

    Basic Quiz Test App. Starting Point

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -12,8 +12,45 @@ class VSJApp extends StatelessWidget
     // TODO: implement build
     return MaterialApp(
 home:Scaffold(
+  appBar: AppBar(
+    backgroundColor: Colors.teal,
+    title: Card(child: Text("Quiz App",style: TextStyle(fontSize: 40,fontWeight: FontWeight.bold,backgroundColor: Colors.teal),)),
+    centerTitle: true,
+  ),
   backgroundColor: Colors.blueAccent,
+body: Container(
+child: Column(
+  crossAxisAlignment: CrossAxisAlignment.stretch,
+  mainAxisAlignment: MainAxisAlignment.end,
+  children: [
 
+    Center(
+      child: Expanded(
+          child:Text("Question Will Come Here",style: TextStyle(fontSize: 24,fontWeight: FontWeight.bold,backgroundColor: Colors.teal),)
+      ),
+    )
+    ,
+    Center(
+      child: Expanded(
+          child:FlatButton(child: Text("Submit True"),onPressed: (){
+            print("Submitted True");
+          },),
+      ),
+    ),
+    Center(
+      child: Expanded(
+          child:FlatButton(child: FlatButton(
+
+            child: Text("Submit False"),
+            onPressed: (){
+              print("Submitted False");
+            },
+          ),)
+      ),
+    )
+  ],
+),
+),
     ),
     );
   }
